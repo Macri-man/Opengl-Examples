3b429fb90f08fe16eca003498af59eb8452bf075
Alexander D. Macri <admfav2012@gmail.com>
2014-03-24 12:26:45 -0400

    Update vertexshader.glsl

--- Drawing-Lights/vertexshader.glsl
+++ Drawing-Lights/vertexshader.glsl
@@ -7,13 +7,9 @@ uniform mat4 modelMatrix;
 in vec3 in_position;
 
 in vec4 in_color;
-in vec3 in_normal;
-
 out vec4 pass_color;
-out vec3 normal;
 
 void main(){
   gl_Position=Matrix*viewMatrix*modelMatrix*vec4(in_position,1.0);
-  normal = in_normal;
   pass_color=in_color;
 }
